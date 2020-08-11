985ecf4948f8e0654ea269804ff92741598db9bf
AndresVoice123 <andres@voice123.com>
2020-08-10 22:41:31 -0500

    done

--- audiogram/initialize-canvas.js
+++ audiogram/initialize-canvas.js
@@ -14,23 +14,27 @@ function initializeCanvas(theme, cb) {
   }
 
   if (theme.backgroundImage) {
-    transports.downloadAudio("images/" + theme.backgroundImage, path.join(__dirname, "..", "settings", "backgrounds", theme.backgroundImage));
+    transports.downloadAudio("images/" + theme.backgroundImage, path.join(__dirname, "..", "settings", "backgrounds", theme.backgroundImage), function(err, data) {
+      if (err) {
+        throw err;
+      }
+      console.log('esta es la data', data);
+      // Load background image from file (done separately so renderer code can work in browser too)
+      fs.readFile(path.join(__dirname, "..", "settings", "backgrounds", theme.backgroundImage), function(err, raw){
+
+        if (err) {
+          return cb(err);
+        }
+
+        var bg = new Canvas.Image;
+        bg.src = raw;
+        renderer.backgroundImage(bg);
+
+        return cb(null, renderer);
+
+      });
+    });
   } 
-
-  // Load background image from file (done separately so renderer code can work in browser too)
-  fs.readFile(path.join(__dirname, "..", "settings", "backgrounds", theme.backgroundImage), function(err, raw){
-
-    if (err) {
-      return cb(err);
-    }
-
-    var bg = new Canvas.Image;
-    bg.src = raw;
-    renderer.backgroundImage(bg);
-
-    return cb(null, renderer);
-
-  });
 
 }
 
