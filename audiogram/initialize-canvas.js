3bc8c7f93b85b1069d4aae5d2ca1110335165a78
AndresVoice123 <andres@voice123.com>
2020-08-13 07:06:46 -0500

    fix local

--- audiogram/initialize-canvas.js
+++ audiogram/initialize-canvas.js
@@ -37,7 +37,22 @@ function initializeCanvas(theme, cb) {
 
       });
     });
-  } 
+  } else {
+    // Load background image from file (done separately so renderer code can work in browser too)
+    fs.readFile(path.join(__dirname, "..", "settings", "backgrounds", theme.backgroundImage), function(err, raw){
+
+      if (err) {
+        return cb(err);
+      }
+
+      var bg = new Canvas.Image;
+      bg.src = raw;
+      renderer.backgroundImage(bg);
+
+      return cb(null, renderer);
+
+    });
+  }
 
 }
 
